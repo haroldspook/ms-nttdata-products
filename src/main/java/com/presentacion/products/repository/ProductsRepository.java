82b93b0d3245f60c672431212d164c00c67c2baf
haroldferrm <haroldferrm@gmail.com>
2023-03-29 19:00:25 -0500

    feat: update ProductsRepository

--- src/main/java/com/presentacion/products/repository/ProductsRepository.java
+++ src/main/java/com/presentacion/products/repository/ProductsRepository.java
@@ -2,7 +2,9 @@ package com.presentacion.products.repository;
 
 import com.presentacion.products.model.entity.Products;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductsRepository extends MongoRepository<Products,Long> {
 
 }
